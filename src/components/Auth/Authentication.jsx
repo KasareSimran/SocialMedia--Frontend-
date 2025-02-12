2cb15380387d7617eccc89acc39234b3e9dae319
Simran Anil Kasare <Simran.Kasare@coforge.com>
2025-02-12 15:08:35 +0530

    adding style

--- src/components/Auth/Authentication.jsx
+++ src/components/Auth/Authentication.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Grid2 }  
-    from '@mui/material'; 
+import {Grid2} from '@mui/material'; 
+import Card from '@mui/material/Card';
+import { Login } from './Login';
 
 export const Authentication = () => {
   return (
@@ -11,13 +12,13 @@ export const Authentication = () => {
         </Grid2>
         <Grid2 item xs={5}>
           <div className='px-20 flex flex-col justify-center h-full'>
-            <div className='card p-8'>
+            <Card className='card p-1'>
               <div className='felx flex-col item-center mb-5 space-y-1'>
-              <h1 className='log'>SocialMedia App</h1>
+              <h1 className='logo text-center'>SocialMedia</h1>
+              <p className='text-center text-sm w-[70&]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
               </div>
-              
-
-            </div>
+              <Login/>
+            </Card>
           </div>
         </Grid2>
       </Grid2>
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Field, Formik } from 'formik'
+import { TextField } from '@mui/material';
 
 export const Login = () => {
+  const [formValue,setFormValue]=useState();
+
+ 
+  const validationSchema={}
+  const initialValues={}
+
+  const handleSubmit=()=>{
+    console.log("handle Submit")
+  }
+
   return (
-    <div>Login</div>
+    <>
+    <Formik onSubmit={handleSubmit} validationSchema={validationSchema} initialValues={initialValues}>
+      <Form className="space-y-5">
+        <div className='space-y-5'>
+          <div>
+            <Field as={TextField} name='email' placeholder='Email' type="email" variant="outline" fullWidth/>
+          </div>
+
+        </div>
+
+      </Form>
+    </Formik>
+    </>
   )
 }
