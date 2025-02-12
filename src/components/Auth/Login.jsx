c116e1c41aef06340b4be90aa8686c87552edbab
Simran Anil Kasare <Simran.Kasare@coforge.com>
2025-02-12 15:57:17 +0530

    add and updated

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from 'react'
-import { Field, Formik } from 'formik'
+import { ErrorMessage, Field, Formik,Form } from 'formik'
 import { TextField } from '@mui/material';
+import * as Yup from 'Yup'
 
 export const Login = () => {
   const [formValue,setFormValue]=useState();
 
  
-  const validationSchema={}
-  const initialValues={}
+  const validationSchema={email:"",password:""}
+  const initialValues={email:Yup.string().email("invalid email").required('Email is required'),
+    password:Yup.string().min(6,"Password must be at least 6 characters").required("Password is required")
+  }
 
   const handleSubmit=()=>{
-    console.log("handle Submit")
-  }
+    console.log("handle Submit");
+  };
 
   return (
     <>
-    <Formik onSubmit={handleSubmit} validationSchema={validationSchema} initialValues={initialValues}>
+    <Formik onSubmit={handleSubmit}
+     validationSchema={validationSchema} 
+     initialValues={initialValues}
+     >
       <Form className="space-y-5">
         <div className='space-y-5'>
           <div>
-            <Field as={TextField} name='email' placeholder='Email' type="email" variant="outline" fullWidth/>
+            <Field as={TextField} name='email' placeholder='Email' type="email" variant="outlined" fullWidth/>
+            <ErrorMessage name='email' component={"div"} className='text-red-500'/>
+          </div>
+          <div>
+            <Field as={TextField} name='password' placeholder='Password' type="password" variant="outlined" fullWidth/>
+            <ErrorMessage name='password' component={"div"} className='text-red-500'/>
           </div>
 
         </div>
