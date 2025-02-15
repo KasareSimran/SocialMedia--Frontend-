1d3e9ed3f34dfeef8e5248442ab77250b3c30e5e
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-02-15 18:48:38 +0530

    added auth

--- src/Redux/Auth/auth.action.jsx
+++ src/Redux/Auth/auth.action.jsx
@@ -1,9 +1,43 @@
-const loginUserAction=(loginData)=>async(dispatcher)=>{
+import axios from "axios"
+import { API_BASE_URL } from "../../config/api"
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS ,REGISTER_SUCCESS,REGISTER_REQUEST,REGISTER_FAILURE} from "./auth.actionType"
+
+export const loginUserAction=(loginData)=>async(dispatch)=>{
+    dispatch({type:LOGIN_REQUEST})
     try{
-        const{data}=
+        const{data}=await axios.post(`${API_BASE_URL}/auth/signin`,loginData.data)
+
+        if(data.jwt){
+            localStorage.setItem("jwt",data.jwt)
+        }
+        console.log("Login in success",data)
+        dispatch({type:LOGIN_SUCCESS,payload:data.jwt})
     }
     catch(error){
-        
+
+        console.log("-------------------",error)
+        dispatch({type:LOGIN_FAILURE,payload:error})
     }
 
-}
+}   
+
+
+
+export const registerUserAction=(loginData)=>async(dispatch)=>{
+    dispatch({type:REGISTER_REQUEST})
+    try{
+        const{data}=await axios.post(`${API_BASE_URL}/auth/signup`,loginData.data)
+
+        if(data.jwt){
+            localStorage.setItem("jwt",data.jwt)
+        }
+        console.log("Register",data)
+        dispatch({type:REGISTER_SUCCESS,payload:data.jwt})
+    }
+    catch(error){
+
+        console.log("-------------------",error)
+        dispatch({type:REGISTER_FAILURE,payload:error})
+    }
+
+}  
--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,7 +1,9 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
+import { authReducer } from "./Auth/auth.reducer";
 
 const rootReducer=combineReducers({
+    auth:authReducer
 
 })
 
