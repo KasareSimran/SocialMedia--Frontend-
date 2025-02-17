5e16613cdf4057d2ec4bee5aedc731b1c56b7b76
Simran Anil Kasare <Simran.Kasare@coforge.com>
2025-02-17 15:38:19 +0530

    updated

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Sidebar } from '../../component/Sidebar'
+import {Route, Routes, useLocation} from 'react-router-dom'
 
 export const Home = () => {
+    const location=useLocation();
   return (
     <div className='px-20'>
-        <Grid>
-            
+        <Grid container spacing={0}>
+            <Grid item xs={0}  lg={3}>
+                <div className='sticky top-0'>
+                    <Sidebar/>
+                </div>
+
+            </Grid>
+            <Grid lg={location.pathname==="/"?6:9   } item className='px-5 flex justify-center' xs={12}>
+            <Routes>
+                <Route path='/' element={MiddlePart}/>
+            </Routes>
+
+            </Grid>
+
         </Grid>
     </div>
   )
