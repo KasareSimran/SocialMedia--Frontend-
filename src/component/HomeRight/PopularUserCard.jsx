41bc303da304877d8ceb56599f373eca65dbbe52
KasareSimran <simrankasare5@gmail.com>
2025-02-19 14:38:42 +0530

    adding famous user card

---
+++ src/component/HomeRight/PopularUserCard.jsx
@@ -0,0 +1,25 @@
+import { Avatar, CardHeader, IconButton } from '@mui/material'
+import React from 'react'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { red } from '@mui/material/colors';
+
+export const PopularUserCard = () => {
+  return (
+    <div>
+          <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+    </div>
+  )
+}
