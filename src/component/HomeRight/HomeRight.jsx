115ae4bc42f7e6383ec74d48d8ca7e7ce093e46c
KasareSimran <simrankasare5@gmail.com>
2025-02-19 14:37:44 +0530

    building structure

--- src/component/HomeRight/HomeRight.jsx
+++ src/component/HomeRight/HomeRight.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { SearchUser } from '../SearchUser/SearchUser'
+import { PopularUserCard } from './PopularUserCard'
 
+
+const popularUser=[]
 export const HomeRight = () => {
   return (
-    <div>HomeRight</div>
+    <div className='pr-5'>
+      <SearchUser/>
+
+      <div className='flex justify-between py-5 items-center'>
+        <p className='font-semibold opacity-70'>Suggestions for you</p>
+        <p className='text-xs font-semibold opacity-95'>View All</p>
+      </div>
+
+      <div className='space-y-5'>
+        <PopularUserCard />  
+      </div>
+    </div>
   )
 }
