d468604c22f31125fe7d8e784021f8da1253f9e2
KasareSimran <simrankasare5@gmail.com>
2025-02-19 14:43:45 +0530

    added follow button

--- src/component/HomeRight/PopularUserCard.jsx
+++ src/component/HomeRight/PopularUserCard.jsx
@@ -1,6 +1,6 @@
-import { Avatar, CardHeader, IconButton } from '@mui/material'
+import { Avatar, Button, CardHeader} from '@mui/material'
 import React from 'react'
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 import { red } from '@mui/material/colors';
 
 export const PopularUserCard = () => {
@@ -13,12 +13,12 @@ export const PopularUserCard = () => {
           </Avatar>
         }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
+          <Button size='small'>
+            Follow
+          </Button>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title="Simran Kasare"
+        subheader="@simranKasare"
       />
     </div>
   )
