3644674997793b9d3a84f8caf5011728616b4f68
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-02-22 19:37:03 +0530

    component created for search user and also added logic

--- src/component/SearchUser/SearchUser.jsx
+++ src/component/SearchUser/SearchUser.jsx
@@ -1,7 +1,42 @@
+import { Avatar, Card, CardHeader } from '@mui/material'
 import React from 'react'
 
 export const SearchUser = () => {
+
+  const handleSearchUser=()=>{
+    console.log("search user...")
+  }
+
+  const handleClick=(id)=>{
+    console.log(id)
+  }
+
   return (
-    <div>SearchUser</div>
+    <div>
+      <div className='py-5 relative'>
+        <input className='bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full' 
+        placeholder='search user...'
+        onChange={handleSearchUser}
+         type='text'/>
+      </div>
+
+
+      {
+        true && <Card>
+          <CardHeader onClick={()=>{
+            handleClick()
+          }}
+          avatar={<Avatar src='https://cdn.pixabay.com/photo/2025/02/11/16/41/swans-9399436_1280.jpg'/>}
+          title='Simran Kasare'
+          subheader={"simrankasare"}
+
+          />
+
+  
+
+        </Card>
+      }
+
+    </div>
   )
 }
