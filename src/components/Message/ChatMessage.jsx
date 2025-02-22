4f3e1c637dfafa767bb0f9428b77e423b4c8a225
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-02-22 20:55:06 +0530

    added the image to message

--- src/components/Message/ChatMessage.jsx
+++ src/components/Message/ChatMessage.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export const ChatMessage = () => {
   return (
-    <div>
-        <div className={`p-1 ${true?"rounded-md":"px-5 rounded-full"} bg-[#191c29]":"bg-[#19]"`}>
-            <p className={`${true?"py-2":"py-1"}`}>message</p>
+    <div className={`flex ${true?"justify-start" :"justify-end"} text-white`}>
+        <div className={`p-1 ${true?"rounded-md":"px-5 rounded-full"} bg-[#191c29]`}>
+            {true && <img className='w-[12rem] h-[17rem] object-cover rounded-md' src='https://cdn.pixabay.com/photo/2025/02/11/16/41/swans-9399436_1280.jpg'/>}
+            <p className={`${true?"py-2":"py-1"}`}>
+                message
+            </p>
         </div>
     </div>
   )
