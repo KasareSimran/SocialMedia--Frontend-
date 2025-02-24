babb6489f1ed9c0cf659e17cd1cabcd80805377f
KasareSimran <139992213+KasareSimran@users.noreply.github.com>
2025-02-24 10:09:57 +0530

    updated and added the dynamic name to user

--- src/component/SearchUser/SearchUser.jsx
+++ src/component/SearchUser/SearchUser.jsx
@@ -1,51 +1,50 @@
 import { Avatar, Card, CardHeader } from '@mui/material'
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { searchUser } from '../../Redux/Auth/auth.action';
 
 export const SearchUser = () => {
 
-  const [userName,setUserName]=useState();
-  const dispatch=useDispatch()
+  const [userName, setUserName] = useState();
+  const dispatch = useDispatch();
+  const { message ,auth} = useSelector(store => store);
 
-  const handleSearchUser=(e)=>{
+
+  const handleSearchUser = (e) => {
     setUserName(e.target.value)
     console.log("search user...")
     dispatch(searchUser(userName))
   }
 
-  const handleClick=(id)=>{
+  const handleClick = (id) => {
     console.log(id)
   }
 
   return (
     <div>
       <div className='py-5 relative'>
-        <input className='bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full' 
-        placeholder='search user...'
-        onChange={handleSearchUser}
-         type='text'/>
+        <input className='bg-transparent border border-[#3b4054] outline-none w-full px-5 py-3 rounded-full'
+          placeholder='search user...'
+          onChange={handleSearchUser}
+          type='text' />
 
-{userName && (
-        <Card className='absolute w-full z-10 top-[4.5rem] cursor-pointer'>
-          <CardHeader onClick={()=>{
-            handleClick()
-            setUserName("");
-          }}
-          avatar={<Avatar src='https://cdn.pixabay.com/photo/2025/02/11/16/41/swans-9399436_1280.jpg'/>}
-          title='Simran Kasare'
-          subheader={"simrankasare"}
+        {userName && (
+          auth.searchUser.map((item) => <Card key={item.id} className='absolute w-full z-10 top-[4.5rem] cursor-pointer'>
+            <CardHeader onClick={() => {
+              handleClick()
+              setUserName("");
+            }}
+              avatar={<Avatar src='https://cdn.pixabay.com/photo/2025/02/11/16/41/swans-9399436_1280.jpg' />}
+              title={item.firstName+" "+item.lastName}
+              subheader={item.firstName.toLowerCase()+""+item.lastName.toLowerCase()}
 
-          />
-
-  
-
-        </Card>
-      )}
+            />
+          </Card>)
+        )}
       </div>
 
 
-     
+
 
     </div>
   )
